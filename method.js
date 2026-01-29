7ff5e8518dc70557a9a7554ff0a2fcf03bf815b6
editeding <editediting458@gmail.com>
2026-01-29 09:41:57 +0530

    [AN][added]

--- method.js
+++ method.js
@@ -23,12 +23,20 @@ console.log("toUpperCase():",str.toUpperCase())
 console.log("toLowerCase():",str.toLowerCase())
 
 console.log("includes():",str.includes("javascript"))
+
 console.log("includes():",str.includes("abc"))
 
 console.log("indexof():",str.indexOf("javascript"))
 
 console.log("Slice():",str.slice(2,-7))
+
 console.log("substring():",str.substring(2,5))
+
 console.log("replece():",str.replace("Hello","hey"))
-console.log("concat():",str.concat("hello javascript"))
-console.log("spilt():",str.split("He"))
+
+console.log("spilt():",str.split(""))
+
+let extraRect=" -HELLO 25";
+console.log("concat():",str.concat("extraRect"))
+
+
