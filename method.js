ef2fd839a8001680e89c5f325653bca15707cad8
editeding <editediting458@gmail.com>
2026-01-29 11:16:22 +0530

    [AN][added]

--- method.js
+++ method.js
@@ -12,7 +12,7 @@ concat()
 spilet()*/
 
 
-let str="Hello"
+/*let str="Hello"
 console.log (str)
 console.log (str.length)
 
@@ -38,5 +38,92 @@ console.log("spilt():",str.split(""))
 
 let extraRect=" -HELLO 25";
 console.log("concat():",str.concat("extraRect"))
+/* array method
+length()
+array
+pop()
+push()
+unshift()
+shift()
+concat()
+includes()
+indexOf()
+splice()
+slice()
+join()
+delete()
+map()
+filter()
+find()
+forEach()
+sort()
+reverse()
+flat()
 
+
+let arr=['one','two','three',1,{name:'abc'}]
+console.log("default array:",arr)
+
+
+console.log("leneth():",arr.length);
+
+//console.log("push()",arr.push("new adde element"))
+arr.push("new adde element")
+console.log("array after push:",arr)
+
+console.log("push()",arr.push("new adde element"))
+
+arr.pop();
+console.log("array after pop:",arr)
+
+arr.unshift("firest elemet")
+console.log("array after unshift:",arr)
+
+arr.shift()
+console.log("array after shift:",arr)
+
+let newArr =['str 1' , 'str 2'];
+let mixedArr = arr.concat(newArr)
+console.log("defult:",newArr)
+console.log("array after concat:",mixedArr)
+
+console.log("include():",arr.includes('one'))
+console.log("include():",arr.includes('two'))
+console.log("include():",arr.includes('three'))
+
+
+
+console.log("indexOf:",arr.indexOf('three'))
+
+console.log("slice:",arr.slice(2,4))
+console.log("splice:",arr.splice(2,4))
+console.log("old array:",arr)
+
+console.log("join:",arr.join(", "))
+
+delete arr[4]
+console.log(arr)*/
+
+console.log("Array iteration method:")
+let numbers=[1,2,3,4,5,6];
+
+console.log("map:",numbers.map(num => num*2))
+console.log("Array map old array :",numbers)
+
+console.log("filter:",numbers.filter(num => num*2))
+console.log("Array filter old array :",numbers)
+
+console.log("forEach:")
+numbers.forEach(num=>console.log(num))
+
+let num=[3,6,5,2,4,1]
+console.log("sort",num.sort())
+
+console.log("reverse()",num.reverse())
+
+let nestedarray =[1,2,[3,4],[5,6]]
+console.log("defult nested array:",nestedarray)
+
+let flatArray=nestedarray.flat()
+console.log("flatarray",flatArray)
 
